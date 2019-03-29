c1ae86dd06127b49f4bfa76959a15ef5492c029d
Alfred Feldmeyer <alfred.feldmeyer@maibornwolff.de>
2019-03-29 15:59:26 +0100

    task1 deploy.sh shows endpoint & aws login url

--- task1/deploy.sh
+++ task1/deploy.sh
@@ -6,7 +6,7 @@ serverless deploy -v
 clear
 echo -e "\n_________________________________\n"
 echo -e "\e[33m\e[4mEndpoints:\e[0m"
-sls info| grep -A 4 POST
+sls info| grep  GET
 
 echo -e "\n_________________________________\n"
 echo -e "\e[33m\e[4mAWS Console Login:\e[0m"
