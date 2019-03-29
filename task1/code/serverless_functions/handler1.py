c2888ab585709d6ae2f1a3cb08f6a2b89d3d082c
Alfred Feldmeyer <alfred.feldmeyer@maibornwolff.de>
2019-03-29 15:59:47 +0100

    task1 removed post request

--- task1/code/serverless_functions/handler1.py
+++ task1/code/serverless_functions/handler1.py
@@ -3,14 +3,14 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
 
-    logger.info("Hello from somewhere in the cloud")
-
+    logger.info("Hello from somewhere in the cloud! This is hello1")
 
     response = {
         "statusCode": 200,
--- task1/code/serverless_functions/handler3.py
+++ task1/code/serverless_functions/handler3.py
@@ -1,10 +1,15 @@
 import json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
     }
+
+    logger.info("Hello from somewhere in the cloud! This is hello3")
 
     response = {
         "statusCode": 200,
