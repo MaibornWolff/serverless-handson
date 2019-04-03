33a31ff2d7110e370668b759f914249172947196
Michael Abey <michael.abey@maibornwolff.de>
2019-04-03 18:43:16 +0200

    mod: optimized for better readability

--- task3/code/serverless_functions/temperature.py
+++ task3/code/serverless_functions/temperature.py
@@ -1,15 +1,13 @@
-def convert_degree_celsius_to_degree_fahrenheit(degree_celsius):
-    return degree_celsius * 9/5 + 32
 
-
-def convert_degree_fahrenheit_to_degree_celsius(degree_fahrenheit):
-    return (degree_fahrenheit - 32) * 5/9
-
-
+"""
+    This serverless method returns the current temperature
+"""
 def temperature(event, context):
+
+    # This value we got from an sensor in fahrenheit
     degree_fahrenheit = 89.6
 
-    responseBody = {
+    response_body = {
         "message": "Temperature retrieved successfully",
         "input": event,
         "output": degree_fahrenheit
@@ -17,7 +15,15 @@ def temperature(event, context):
 
     response = {
         "statusCode": 200,
-        "body": responseBody
+        "body": response_body
     }
 
     return response
+
+
+
+def convert_celsius_to_fahrenheit(degree_celsius):
+    return degree_celsius * 9/5 + 32
+
+def convert_fahrenheit_to_celsius(degree_fahrenheit):
+    return (degree_fahrenheit - 32) * 5/9
