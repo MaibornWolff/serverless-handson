6e15359f9b9a8fe7094a04b2b43908bfc1ede7c0
Valentin Hauner <valentin.hauner@maibornwolff.de>
2019-04-05 16:07:53 +0200

    Adds deploy.sh to task2

---
+++ task2/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+cd code
+serverless deploy -v
+
+clear
+echo -e "\n_________________________________\n"
+echo -e "\e[33m\e[4mEndpoints:\e[0m"
+sls info| grep  GET
+
+echo -e "\n_________________________________\n"
+echo -e "\e[33m\e[4mAWS Console Login:\e[0m"
+AWS_ACCOUNT=$(aws iam list-users | cut -d ':' -f5)
+echo -e " url: https://${AWS_ACCOUNT}.signin.aws.amazon.com/console/"
+
+
+
+
+
+cd - > /dev/null
