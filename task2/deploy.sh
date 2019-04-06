91f90ec39eca57964a7222750d3888e08413cb5c
Alfred Feldmeyer <alfred.feldmeyer@maibornwolff.de>
2019-04-06 12:24:35 +0200

    Added output format to deploy script

--- task2/deploy.sh
+++ task2/deploy.sh
@@ -10,7 +10,7 @@ sls info| grep  GET
 
 echo -e "\n_________________________________\n"
 echo -e "\e[33m\e[4mAWS Console Login:\e[0m"
-AWS_ACCOUNT=$(aws iam list-users | cut -d ':' -f5)
+AWS_ACCOUNT=$(aws iam list-users  --output text | cut -d ':' -f5)
 echo -e " url: https://${AWS_ACCOUNT}.signin.aws.amazon.com/console/"
 
 
