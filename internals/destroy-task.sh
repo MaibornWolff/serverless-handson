36abd28b8e2b212ee9a0a23d83192f8856dca86d
Alfred Feldmeyer <alfred.feldmeyer@maibornwolff.de>
2019-04-06 11:24:33 +0200

    remove unused bash scripts

--- internals/destroy-task.sh
+++ internals/destroy-task.sh
@@ -6,9 +6,25 @@ if [[ $# -eq 0 ]]; then
 fi
 
 case "${1}" in
-    1) cd ../task1/code && serverless remove && cd - ;;
-    2) ;;
-    3) ;;
-    4) ;;
-    5) cd ../task5/code && serverless client remove --no-confirm && serverless remove && cd - ;;
+    1)
+        cd ../task1/code
+        serverless remove
+        cd -
+        rm -rf ../task1/code/.serverless
+    ;;
+    2)
+    ;;
+    3)
+    ;;
+    4)
+    ;;
+    5)
+        cd ../task5/code
+        serverless client remove --no-confirm
+        serverless remove
+        cd -
+        rm -rf ../task5/code/.serverless
+        rm -rf ../task5/code/node_modules
+        rm -rf ../task5/frontend/node_modules
+    ;;
 esac
