da23a8d039e6217bf7304a7c136868e4f70b01b0
Alfred Feldmeyer <alfred.feldmeyer@maibornwolff.de>
2019-04-07 11:45:43 +0200

    fixed bad trimming

--- task5/sha.js
+++ task5/sha.js
@@ -2,5 +2,12 @@ const crypto = require("crypto");
 var shell = require('./code/node_modules/shelljs');
 
 module.exports = (serverless) => {
-    return shell.exec("aws iam list-users --output json| jq '.Users[0].UserName'| tr -d \\\"",{silent:true}).stdout
+    var username=shell.exec("aws iam list-users --output json| jq '.Users[0].UserName'|tr -d \\\"",{silent:true})
+    .stdout.trim();
+
+    if(username==""){
+        console.log("No AWS Profile set")
+        process.exit(1)
+    }
+    return username
 }
