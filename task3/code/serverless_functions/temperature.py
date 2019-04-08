eb16a96e1cb8b8ce333b7a163f29e0df25be788f
Michael Abey <michael.abey@maibornwolff.de>
2019-04-08 16:01:10 +0200

    mod: slides

--- task3/code/serverless_functions/temperature.py
+++ task3/code/serverless_functions/temperature.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from common.generate_log import monitor
+from common.generate_log import send_to_monitor
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -10,21 +10,24 @@ logger.setLevel(logging.INFO)
 """
 def temperature(event, context):
 
-    temperature_kelvin = 305.15
-    logger.info("This is the current temperature from a sensor: "+str(temperature_kelvin)+" in kelvin")
+    #  v There is a bug below this line v
+    sensor_value_in_kelvin = 305.15
+    logger.info("This is the current temperature from a sensor: "+str(sensor_value_in_kelvin)+" in kelvin")
 
 
-    temperature = convert_kelvin_to_fahrenheit(temperature_kelvin)
-    logger.info("This is the converted temperature: "+str(temperature))
+    temperature_in_celcius = convert_kelvin_to_fahrenheit(sensor_value_in_kelvin)
+    logger.info("This is the converted temperature: "+str(temperature_in_celcius))
+    #  ^ There is a bug above this line ^
 
 
-    monitor("Sent temperature to monitoring", temperature)
 
+    send_to_monitor("Sent temperature to monitoring", temperature_in_celcius)
 
+    #TODO: MIAB - response one line
     response_body = {
         "message": "Temperature retrieved successfully",
         "input": event,
-        "output": temperature
+        "output": temperature_in_celcius
     }
 
     response = {
