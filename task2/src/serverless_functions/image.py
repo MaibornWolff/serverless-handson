4d37647f5084ee4be8000f67597cf5db5c97c331
Marcel Henrich <marcel.henrich@maibornwolff.de>
2020-06-25 16:16:59 +0200

    removed solution from task for attendees

--- task2/src/serverless_functions/image.py
+++ task2/src/serverless_functions/image.py
@@ -1,51 +1,14 @@
-import boto3
-from common import image_helper
-from common import logger
-from common import response
-
-s3_client = boto3.client('s3')
-s3_bucket = boto3.resource('s3')
-
-BUCKET_NAME = "mw-serverless-cloud-school"
-
-
 def get_upload_form(event, context):
-    return response.html_upload_form()
+    return
 
 
 def upload_image(event, context):
-    try:
-        uploaded_images = image_helper.extract_uploaded_files(event)
-
-        for uploaded_image in uploaded_images:
-            s3_response = s3_client.put_object(Bucket=BUCKET_NAME, Key=uploaded_image[0], Body=uploaded_image[1])
-            logger.info(f'Uploaded image {uploaded_image[0]} successfully: {s3_response}')
-
-        return response.redirect('list')
-    except Exception as e:
-        logger.info(f'Failed to upload image {uploaded_image[0]}: {str(e)}')
-        return response.create(str(e), 500)
+    return
 
 
 def get_image(event, context):
-    try:
-        image_name = event["queryStringParameters"]["name"]
-        image_data = s3_client.get_object(Bucket=BUCKET_NAME, Key=image_name)
-
-        return image_helper.encode_image(image_name, image_data)
-    except Exception as e:
-        return response.create(str(e), 500)
+    return
 
 
 def list_images(event, context):
-    try:
-        images = s3_bucket.Bucket(BUCKET_NAME).objects.all()
-
-        images_markup = [
-            f'<li><img alt="{image.key}" src="get?name={image.key}" /></li>'
-            for image in images
-        ]
-
-        return response.html('Images', f"<ul>{''.join(images_markup)}</ul>")
-    except Exception as e:
-        return response.create(str(e), 500)
+    return
