fef2d03546803a3affcf70edfefd08c392ba649e
Valentin Hauner <valentin.hauner@maibornwolff.de>
2022-04-05 11:19:14 +0000

    Provides support for task 1.3

--- task1/src/serverless_functions/image.py
+++ task1/src/serverless_functions/image.py
@@ -3,12 +3,16 @@ from common import image_helper
 from common import logger
 from common import response
 
+s3_client = boto3.client('s3')
+
 
 def get_upload_form(event, context):
     return response.html_upload_form()
 
 
 def upload_image(event, context):
+    # First step: Extract the uploaded files out of the event
+    # Second step: Use s3_client.put_object(BUCKET_NAME, OBJECT_KEY, OBJECT_BODY) to finally upload each uploaded file
     return
 
 
